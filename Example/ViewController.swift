1e2080ac696bde7289c979e878fbe609986d2e7c
Lasha Efremidze <efremidzel@hotmail.com>
2017-12-14 21:04:38 -0800

    Updated Gradient

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -48,10 +48,31 @@ extension HolographicView {
 }
 
 extension HolographicView {
-    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        UIView.animate(withDuration: 0.2, animations: { self.transform = .identity })
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        animate(didEnter: true)
     }
-    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        UIView.animate(withDuration: 0.2, animations: { self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9) })
+    open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let locations = [touch.previousLocation(in: self), touch.location(in: self)].map { self.bounds.contains($0) }
+        let isEntering = !locations[0] && locations[1]
+        let isExiting = locations[0] && !locations[1]
+        if isEntering {
+            animate(didEnter: true)
+        } else if isExiting {
+            animate(didEnter: false)
+        }
+    }
+    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        animate(didEnter: false)
+    }
+    open override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        animate(didEnter: false)
+    }
+    func animate(didEnter: Bool) {
+        if didEnter {
+            UIView.animate(withDuration: 0.2, animations: { self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95) })
+        } else {
+            UIView.animate(withDuration: 0.2, animations: { self.transform = .identity })
+        }
     }
 }
