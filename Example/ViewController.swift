8e9a0f95dcc0eab14acdce05ffa8e0aa6d645cde
Lasha Efremidze <efremidzel@hotmail.com>
2017-12-18 11:38:27 -0800

    Updated Demo

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var holographicView: HolographicView! {
         didSet {
+            holographicView.colors = UIColor.all
             holographicView.layer.cornerRadius = 20
             holographicView.layer.masksToBounds = true
         }
@@ -78,4 +79,19 @@ extension UIInterpolatingMotionEffect {
         self.minimumRelativeValue = -span
         self.maximumRelativeValue = span
     }
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
+    }
+    class var red: UIColor { return UIColor(red: 255, green: 59, blue: 48) }
+    class var orange: UIColor { return UIColor(red: 255, green: 149, blue: 0) }
+    class var yellow: UIColor { return UIColor(red: 255, green: 204, blue: 0) }
+    class var green: UIColor { return UIColor(red: 76, green: 217, blue: 100) }
+    class var tealBlue: UIColor { return UIColor(red: 90, green: 200, blue: 250) }
+    class var blue: UIColor { return UIColor(red: 0, green: 122, blue: 255) }
+    class var purple: UIColor { return UIColor(red: 88, green: 86, blue: 214) }
+    class var pink: UIColor { return UIColor(red: 255, green: 45, blue: 85) }
+    static let all: [UIColor] = [red, orange, yellow, green, tealBlue, blue, purple, pink]
 }
--- Sources/Holographic.swift
+++ Sources/Holographic.swift
@@ -6,21 +6,30 @@
 //  Copyright © 2017 Lasha Efremidze. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import CoreMotion
 
 @IBDesignable
 open class HolographicView: UIView {
-    private let gradient = RadialGradientLayer()
+    
+    private lazy var gradientLayer: RadialGradientLayer = {
+        let gradientLayer = RadialGradientLayer()
+        gradientLayer.needsDisplayOnBoundsChange = true
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        return gradientLayer
+    }()
+    
     private let motionManager = CMMotionManager()
+    
+    open var colors = [UIColor]() {
+        didSet {
+            gradientLayer.colors = colors.map { $0.cgColor }
+            gradientLayer.frame = self.bounds.insetBy(dx: -self.bounds.width * CGFloat(colors.count), dy: -self.bounds.height * CGFloat(colors.count))
+        }
+    }
     
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
-        gradient.needsDisplayOnBoundsChange = true
-        gradient.frame = self.bounds.insetBy(dx: -self.bounds.width * CGFloat(UIColor.all.count), dy: -self.bounds.height * CGFloat(UIColor.all.count))
-        gradient.colors = UIColor.all.map { $0.cgColor }
-        self.layer.insertSublayer(gradient, at: 0)
         
         guard motionManager.isDeviceMotionAvailable else { return }
         motionManager.deviceMotionUpdateInterval = 0.1
@@ -33,10 +42,11 @@ open class HolographicView: UIView {
                 return CGFloat(max(offset, min(0.5 - (position * multiplier / 0.5), 1 - offset)))
             }
             
-            self.gradient.anchorPoint.x = getPosition(data.gravity.x)
-            self.gradient.anchorPoint.y = getPosition(data.gravity.y)
+            self.gradientLayer.anchorPoint.x = getPosition(data.gravity.x)
+            self.gradientLayer.anchorPoint.y = getPosition(data.gravity.y)
         }
     }
+    
 }
 
 class RadialGradientLayer: CALayer {
@@ -49,29 +59,14 @@ class RadialGradientLayer: CALayer {
     }
 }
 
-class RadialGradientView: UIView {
-    override class var layerClass: AnyClass {
-        return RadialGradientLayer.self
-    }
-}
+//class RadialGradientView: UIView {
+//    override class var layerClass: AnyClass {
+//        return RadialGradientLayer.self
+//    }
+//}
 
 extension CALayer {
     var radius: CGFloat {
         return sqrt(pow(bounds.width / 2, 2) + pow(bounds.height / 2, 2))
     }
-}
-
-extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int) {
-        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
-    }
-    class var red: UIColor { return UIColor(red: 255, green: 59, blue: 48) }
-    class var orange: UIColor { return UIColor(red: 255, green: 149, blue: 0) }
-    class var yellow: UIColor { return UIColor(red: 255, green: 204, blue: 0) }
-    class var green: UIColor { return UIColor(red: 76, green: 217, blue: 100) }
-    class var tealBlue: UIColor { return UIColor(red: 90, green: 200, blue: 250) }
-    class var blue: UIColor { return UIColor(red: 0, green: 122, blue: 255) }
-    class var purple: UIColor { return UIColor(red: 88, green: 86, blue: 214) }
-    class var pink: UIColor { return UIColor(red: 255, green: 45, blue: 85) }
-    static let all: [UIColor] = [red, orange, yellow, green, tealBlue, blue, purple, pink]
 }
