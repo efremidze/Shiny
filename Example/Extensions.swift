32db904df0ee75e9429eb5a9cd1daf6240d6cc89
Lasha Efremidze <lasha.efremidze@ring.com>
2017-12-19 20:48:14 -0800

    Updated Demo

---
+++ Example/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  Example
+//
+//  Created by Lasha Efremidze on 12/19/17.
+//  Copyright © 2017 Lasha Efremidze. All rights reserved.
+//
+
+import UIKit
+
+extension UIInterpolatingMotionEffect {
+    convenience init(keyPath: String, type: UIInterpolatingMotionEffectType, span: Int) {
+        self.init(keyPath: keyPath, type: type)
+        self.minimumRelativeValue = -span
+        self.maximumRelativeValue = span
+    }
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
+    }
+    class var red: UIColor { return UIColor(red: 255, green: 59, blue: 48) }
+    class var orange: UIColor { return UIColor(red: 255, green: 149, blue: 0) }
+    class var yellow: UIColor { return UIColor(red: 255, green: 204, blue: 0) }
+    class var green: UIColor { return UIColor(red: 76, green: 217, blue: 100) }
+    class var tealBlue: UIColor { return UIColor(red: 90, green: 200, blue: 250) }
+    class var blue: UIColor { return UIColor(red: 0, green: 122, blue: 255) }
+    class var purple: UIColor { return UIColor(red: 88, green: 86, blue: 214) }
+    class var pink: UIColor { return UIColor(red: 255, green: 45, blue: 85) }
+    class var background: UIColor { return UIColor(red: 17, green: 17, blue: 17) }
+    class var gray: UIColor { return UIColor(red: 34, green: 34, blue: 34) }
+    static let all: [UIColor] = [red, orange, yellow, green, tealBlue, blue, purple, pink, gray]
+}
