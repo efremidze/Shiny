2a9db67e280069755ef21cff42d355b0b7bc51a2
Lasha Efremidze <efremidzel@hotmail.com>
2018-01-14 00:30:53 -0800

    Updated Gyro

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -6,10 +6,19 @@
 //  Copyright © 2017 Lasha Efremidze. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 infix operator %: MultiplicationPrecedence
 func % (left: CGFloat, right: CGFloat) -> CGFloat {
     let v = left.truncatingRemainder(dividingBy: right)
     return v >= 0 ? v : v + right
+}
+
+extension UIImage {
+    convenience init(from view: UIView) {
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        self.init(cgImage:(UIGraphicsGetImageFromCurrentImageContext()?.cgImage!)!)
+        UIGraphicsEndImageContext()
+    }
 }
