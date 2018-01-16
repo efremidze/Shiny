c6046e1b2c35c81b08333a6d2d0e94404426a520
Lasha Efremidze <efremidzel@hotmail.com>
2018-01-15 17:16:50 -0800

    Adding Sphere

--- Sources/Shiny.swift
+++ Sources/Shiny.swift
@@ -8,81 +8,59 @@
 
 import UIKit
 import CoreMotion
+import SceneKit
 
-//@IBDesignable
 open class ShinyView: UIView {
     
-    lazy var horizontalLayer: CAReplicatorLayer = {
-        let replicatorLayer = CAReplicatorLayer()
-        replicatorLayer.frame.size = size
-        replicatorLayer.instanceCount = Int(ceil(size.width / itemSize.width))
-        replicatorLayer.instanceTransform = CATransform3DMakeTranslation(itemSize.width, 0, 0)
-        return replicatorLayer
+    let cameraNode = SCNNode()
+    
+    lazy var sphere: SCNSphere = {
+        let sceneView = SCNView(frame: self.bounds)
+        self.addSubview(sceneView)
+        
+        // Set the scene
+        let scene = SCNScene()
+        sceneView.scene = scene
+        sceneView.allowsCameraControl = true
+        
+        // Create node, containing a sphere, using the panoramic image as a texture
+        let sphere = SCNSphere(radius: 20.0)
+        sphere.firstMaterial!.isDoubleSided = true
+//        sphere.firstMaterial!.diffuse.contents = image
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3Make(0, 0, 0)
+        scene.rootNode.addChildNode(sphereNode)
+        
+        // Camera, ...
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(0, 0, 0)
+        scene.rootNode.addChildNode(cameraNode)
+        
+        return sphere
     }()
     
-    lazy var verticalLayer: CAReplicatorLayer = {
-        let replicatorLayer = CAReplicatorLayer()
-        replicatorLayer.frame.size = itemSize
-//        replicatorLayer.position = CGPoint(x: -itemSize.width / 2, y: -itemSize.height / 2)
-//        replicatorLayer.frame = self.frame.insetBy(dx: -size.width / 2, dy: -size.height / 2)
-        replicatorLayer.instanceCount = Int(ceil(size.height / itemSize.height))
-        replicatorLayer.instanceTransform = CATransform3DMakeTranslation(0, itemSize.height, 0)
-        return replicatorLayer
-    }()
-    
-    lazy var gradientLayer: RadialGradientLayer = {
-        let gradientLayer = RadialGradientLayer()
-        gradientLayer.frame.size = itemSize
-        gradientLayer.needsDisplayOnBoundsChange = true
-        gradientLayer.backgroundColor = UIColor.clear.cgColor
-//        self.layer.insertSublayer(verticalLayer, at: 0) // TEMP
-        self.layer.addSublayer(verticalLayer)
-        verticalLayer.addSublayer(horizontalLayer)
-        horizontalLayer.addSublayer(gradientLayer)
-        return gradientLayer
-    }()
-    
-    var size: CGSize {
-        return CGSize(width: itemSize.width * 4, height: itemSize.height * 4)
-    }
-    
-    var itemSize: CGSize {
-        let dimension = min(self.frame.width, self.frame.height)
-        return CGSize(width: dimension, height: dimension)
-    }
-    
-    // width / 3 +- 0.5
-    
-    /**
-     The array of UIColor objects defining the color of each gradient stop.
-     */
     open var colors = [UIColor]() {
         didSet {
-            gradientLayer.colors = colors.map { $0.cgColor }
-//            replicatorLayer.frame = self.bounds.insetBy(dx: -self.bounds.width * CGFloat(colors.count) * spread, dy: -self.bounds.height * CGFloat(colors.count) * spread)
+//            view.gradientLayer.colors = colors.map { $0.cgColor }
         }
     }
-    
-    /**
-     The array of CGFloat objects defining the location of each gradient stop as a value in the range [0,1]. The values must be monotonically increasing. If a nil array is given, the stops are assumed to spread uniformly across the [0,1] range. Defaults to nil.
-     */
-    open var locations: [CGFloat]? {
-        get { return gradientLayer.locations }
-        set { gradientLayer.locations = newValue }
-    }
-    
-    open var rollOffset: CGFloat = 0
-    open var pitchOffset: CGFloat = 0
     
     /**
      Starts listening to motion updates.
      */
     open func startUpdates() {
-        self.verticalLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        let view = ReplicatorLayerView(frame: self.bounds)
+        view._layer.gradientLayer.colors = colors.map { $0.cgColor }
+        let image = UIImage(from: view)
+        sphere.firstMaterial!.diffuse.contents = image
+        
+        // 6 gradients
+        // (1, 0, 0), (0, 1, 0), (0, 0, 1)
+        // (-1, 0, 0), (0, -1, 0), (0, 0, -1)
+
         Gyro.observe { [weak self] roll, pitch, yaw in
             guard let `self` = self else { return }
-            self.verticalLayer.anchorPoint.x = ((180 + roll + self.rollOffset) % 360) / 360
-            self.verticalLayer.anchorPoint.y = ((180 + pitch + self.pitchOffset) % 360) / 360
+            self.cameraNode.eulerAngles = SCNVector3(x: Float(roll), y: Float(yaw), z: Float(pitch))
         }
     }
     
